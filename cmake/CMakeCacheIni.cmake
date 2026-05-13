ae40a613750a475db9a07e7e127372c3bc791836
windroadDevCom <abuwindroad@163.com>
2026-05-13 15:42:53 +0800

    添加 AddThirdSDKTargets.cmake 文件并更新 CMakeLists.txt 以包含该文件

--- cmake/CMakeCacheIni.cmake
+++ cmake/CMakeCacheIni.cmake
@@ -2,6 +2,21 @@ set(QT_DIR "D:" CACHE PATH "Path to Qt installation")
 set(RWUL_DIR "D:" CACHE PATH "Path to RWUL installation")
 set(HALCON_DIR "D:" CACHE PATH "Path to Halcon installation")
 
+message(STATUS "QT_DIR : ${QT_DIR}")
+if(NOT EXISTS "${QT_DIR}")
+    message(WARNING "QT_DIR does not exist: ${QT_DIR}")
+endif()
+
+message(STATUS "RWUL_DIR : ${RWUL_DIR}")
+if(NOT EXISTS "${RWUL_DIR}")
+    message(WARNING "RWUL_DIR does not exist: ${RWUL_DIR}")
+endif()
+
+message(STATUS "HALCON_DIR : ${HALCON_DIR}")
+if(NOT EXISTS "${HALCON_DIR}")
+    message(WARNING "HALCON_DIR does not exist: ${HALCON_DIR}")
+endif()
+
 list(APPEND CMAKE_PREFIX_PATH ${QT_DIR})
 list(APPEND CMAKE_PREFIX_PATH ${RWUL_DIR})
 list(APPEND CMAKE_PREFIX_PATH ${HALCON_DIR})
