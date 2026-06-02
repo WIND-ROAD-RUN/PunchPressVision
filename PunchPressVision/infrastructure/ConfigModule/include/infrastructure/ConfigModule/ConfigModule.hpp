0451796d0aa82e05e82236a929699f55981b5d5a
zfkjCompile <zfkjCompile@DESKTOP-UGOT9GT>
2026-06-02 15:54:19 +0800

    集成 RWUL::oso 组件并扩展 ConfigModule
    本次提交在 CMakeLists.txt 中集成 RWUL::oso 组件，配置相关路径并链接库。同时，扩展 ConfigModule，增加 StorageContext 成员及相关方法，为后续功能开发打下基础。

--- PunchPressVision/infrastructure/ConfigModule/include/infrastructure/ConfigModule/ConfigModule.hpp
+++ PunchPressVision/infrastructure/ConfigModule/include/infrastructure/ConfigModule/ConfigModule.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
+#include "rwul/oso/oso_StorageContext.hpp"
+
 namespace inf
 {
 	class ConfigModule
 	{
 	public:
-		
+		ConfigModule();
+		~ConfigModule();
+	private:
+		std::unique_ptr<rw::oso::StorageContext> storageContext_;
+	public:
+		void build();
+		void destroy();
 	};
 }
