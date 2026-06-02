9970ad22e013e54949f9fc0ee124d226a9c2963d
zfkjCompile <zfkjCompile@DESKTOP-UGOT9GT>
2026-06-02 16:45:15 +0800

    新增全局路径配置及相关依赖引入
    在 GlobalPath.hpp 中添加了 ProjectRootPath 变量用于统一管理项目根路径；CMakeLists.txt 为 Inf_TARGET 增加了 global::global 依赖；ConfigModulePath.hpp 引入了全局路径头文件，便于模块间路径共享。

--- PunchPressVision/global/include/global/GlobalPath.hpp
+++ PunchPressVision/global/include/global/GlobalPath.hpp
@@ -1,6 +1,7 @@
 #pragma once
+#include <string>
 
 namespace global
 {
-	
+	inline std::string ProjectRootPath = "D:/zfkjData/PunchPressVision";
 }
--- PunchPressVision/infrastructure/ConfigModule/include/infrastructure/ConfigModule/ConfigModulePath.hpp
+++ PunchPressVision/infrastructure/ConfigModule/include/infrastructure/ConfigModule/ConfigModulePath.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "global/GlobalPath.hpp"
+
 namespace inf
 {
 
