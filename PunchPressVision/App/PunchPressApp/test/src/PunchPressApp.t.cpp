f4d6dcd162d05677d819c7bcb575ffa310cf0c82
windroadDevCom <abuwindroad@163.com>
2026-06-13 13:57:06 +0800

    初始化 PunchPressApp 应用启动流程
    在 PunchPressApp.t.cpp 中引入 PunchPressApp 相关头文件，并在 main 函数中依次完成基础设施、业务逻辑及应用对象的创建、构建和启动，最后启动 Qt 应用主循环，实现应用的完整初始化流程。

--- PunchPressVision/App/PunchPressApp/test/src/PunchPressApp.t.cpp
+++ PunchPressVision/App/PunchPressApp/test/src/PunchPressApp.t.cpp
@@ -1,10 +1,22 @@
 #include "PunchPressApp.t.hpp"
 
+#include "app/PunchPressApp.hpp"
+
 #include <QCoreApplication>
 
 int main(int argc, char* argv[])
 {
 	QCoreApplication app(argc, argv);
+
+	inf::infrastructure inf;
+	inf.build();
+	bun::Business business(inf);
+	business.build();
+	business.start();
+	app::PunchPressApp punchPressApp(business);
+	punchPressApp.build();
+	punchPressApp.start();
+
 
 	return app.exec();
 }
